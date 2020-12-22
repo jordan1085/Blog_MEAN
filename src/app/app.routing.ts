5be6cde453ec52cbb4866f70e9866c8067b9c1a2
Jordan <jordanhernando1085@gmail.com>
2020-12-22 12:06:45 -0300

    Routing con angular

---
+++ src/app/app.routing.ts
@@ -0,0 +1,26 @@
+// Importando los modulos del router de angular
+import { ModuleWithProviders } from '@angular/core';
+import {Routes, RouterModule } from '@angular/router';
+
+// Importar componentes que seran una pagina propia
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+
+// Array de rutas
+const appRoutes: Routes = [
+
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'peliculas', component: PeliculasComponent},
+    {path: 'pagina-de-pruebas', component: PaginaComponent}
+
+];
+
+// Exportar el modulo de rutas
+export const appRoutingProviders: any[] = []; // Establecer como servicio
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes); // Cargar la configuracion de rutas
